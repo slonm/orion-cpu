db600489445a7497dacedea8ab166c038a0dab61
slobodyanukma <slobodyanukma@3978162c-2b8c-11df-9d00-8333cd69f872>
2010-04-21 10:59:53 +0000

    update

--- cpu/cpu-units/src/main/java/orion/cpu/services/UnitsInitializeDatabase.java
+++ cpu/cpu-units/src/main/java/orion/cpu/services/UnitsInitializeDatabase.java
@@ -1,8 +1,11 @@
 package orion.cpu.services;
 
+import br.com.arsmachina.controller.Controller;
 import orion.cpu.security.OperationTypes;
 import orion.cpu.services.impl.InitializeDatabaseSupport;
 import ua.mihailslobodyanuk.utils.Defense;
+import orion.cpu.entities.org.*;
+import java.util.List;
 
 /**
  * Начальная инициализация данных.
@@ -10,13 +13,45 @@ import ua.mihailslobodyanuk.utils.Defense;
  */
 public class UnitsInitializeDatabase extends OperationTypes implements Runnable {
 
-    private final InitializeDatabaseSupport initDBSupport;
+    private final InitializeDatabaseSupport iDBSpt;
 
     public UnitsInitializeDatabase(InitializeDatabaseSupport initializeDatabaseSupport) {
-        this.initDBSupport = Defense.notNull(initializeDatabaseSupport, "initializeDatabaseSupport");
+        this.iDBSpt = Defense.notNull(initializeDatabaseSupport, "initializeDatabaseSupport");
+        //--Создание контроллера, работающего с экземплярами подразделений
     }
 
     @Override
     public void run() {
+//        //Заполнение тестовых данных
+        if (iDBSpt.isFillTestData()) {
+//        //---Кафедры, выполняющие обучение по лицензиям----------
+            Chair kafPIT = saveOrUpdateChair("кафедра програмування та інформаційних технологій", "КПІТ");
+            Chair kafEICPHS = saveOrUpdateChair("кафедра управління навчальними закладами та педагогіки вищої школи", "КУНЗПВШ");
+        }
+    }
+
+    public Chair saveOrUpdateChair(String name, String shortName) {
+        Controller<Chair, Integer> ouController;
+        ouController = iDBSpt.getControllerSource().get(Chair.class);
+        //---Создание образца записи кафедры - наследника абстрактного класса Kafedra
+        Chair ouSample = new Chair();
+        ouSample.setName(name);
+        ouSample.setShortName(shortName);
+
+        //--Выборка списка данных записи кафедры
+        List<Chair> ou = ouController.findByExample(ouSample);
+
+        //--Переменная для работы с экземпляром элемента спискка
+        Chair p;
+        //--Инициализация элемента списка
+        if (ou.size() == 0) {
+            p = new Chair();
+            p.setName(name);
+            p.setShortName(shortName);
+
+        } else {
+            p = ou.get(0);
+        }
+        return ouController.saveOrUpdate(p);
     }
 }
