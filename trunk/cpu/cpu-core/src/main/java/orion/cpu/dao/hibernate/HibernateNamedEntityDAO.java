21ddcd242f5a65e6c306d8d61e5b74d14ce788f0
slobodyanukma <slobodyanukma@3978162c-2b8c-11df-9d00-8333cd69f872>
2010-04-22 10:48:01 +0000

    Багофикс

--- trunk/cpu/cpu-core/src/main/java/orion/cpu/dao/hibernate/HibernateNamedEntityDAO.java
+++ trunk/cpu/cpu-core/src/main/java/orion/cpu/dao/hibernate/HibernateNamedEntityDAO.java
@@ -1,6 +1,7 @@
 package orion.cpu.dao.hibernate;
 
-import br.com.arsmachina.dao.hibernate.ConcreteDAOImpl;
+import br.com.arsmachina.authentication.dao.hibernate.Constants;
+import br.com.arsmachina.dao.SortCriterion;
 import java.util.List;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
@@ -24,5 +25,15 @@ public class HibernateNamedEntityDAO<T extends NamedEntity<?>> extends SecuredHi
     @Override
     public List<T> findByName(String name) {
         return createCriteria().add(Restrictions.eq("name", name)).list();
+    }
+
+    /**
+     * Returns {@link Constants#ASCENDING_NAME_SORT_CRITERIA}.
+     *
+     * @see br.com.arsmachina.dao.hibernate.GenericDAOImpl#getDefaultSortCriteria()
+     */
+    @Override
+    public SortCriterion[] getDefaultSortCriteria() {
+        return Constants.ASCENDING_NAME_SORT_CRITERIA;
     }
 }
