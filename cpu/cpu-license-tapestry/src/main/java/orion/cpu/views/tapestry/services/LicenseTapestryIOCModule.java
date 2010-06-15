553a9300a6e2b4fa19be664570e64d42ac202c8a
slobodyanukma <slobodyanukma@3978162c-2b8c-11df-9d00-8333cd69f872>
2010-06-15 16:52:42 +0000

    Коррекции всвязи с добавлением обработчика ошибок авторизации Багофиксы

--- cpu/cpu-license-tapestry/src/main/java/orion/cpu/views/tapestry/services/LicenseTapestryIOCModule.java
+++ cpu/cpu-license-tapestry/src/main/java/orion/cpu/views/tapestry/services/LicenseTapestryIOCModule.java
@@ -2,6 +2,7 @@ package orion.cpu.views.tapestry.services;
 
 import br.com.arsmachina.tapestrycrud.services.TapestryCrudModuleService;
 import org.apache.tapestry5.ioc.*;
+import org.apache.tapestry5.ioc.services.Coercion;
 import org.apache.tapestry5.services.*;
 import orion.cpu.baseentities.BaseEntity;
 import orion.cpu.entities.ref.EducationForm;
@@ -12,6 +13,7 @@ import orion.cpu.entities.uch.License;
 import orion.cpu.entities.uch.LicenseRecord;
 import orion.cpu.entities.uch.LicenseRecordView;
 import orion.cpu.views.tapestry.pages.ListView;
+import orion.cpu.views.tapestry.pages.uch.ListLicenseRecordView;
 import orion.tapestry.menu.lib.IMenuLink;
 import orion.tapestry.menu.lib.PageMenuLink;
 
@@ -50,7 +52,7 @@ public class LicenseTapestryIOCModule {
         configuration.add(path, createPageMenuLink(tcms, LicenseRecord.class));
 
         path = "Start>LicenseRecordView";
-        configuration.add(path, createPageMenuLink(tcms, LicenseRecordView.class));
+        configuration.add(path, new PageMenuLink(tcms.getListPageClass(LicenseRecordView.class)));
 
         path = "Start>LicenseRecordView>EducationForm";
         configuration.add(path, createPageMenuLink(tcms, EducationForm.class));
@@ -69,5 +71,17 @@ public class LicenseTapestryIOCModule {
     }
     public static void contributeGlobalMessageAppender(OrderedConfiguration<String> configuration){
         configuration.add("License", "classpath:License.properties");
+    }
+
+    public static void contributeMetaLinkCoercion(Configuration<Coercion> configuration) {
+        configuration.add(new Coercion<IMenuLink, Class<LicenseRecordView>>(){
+
+            @Override
+            public Class<LicenseRecordView> coerce(IMenuLink input) {
+                if(input.getPageClass().equals(ListLicenseRecordView.class))
+                    return LicenseRecordView.class;
+                return null;
+            }
+        });
     }
 }
