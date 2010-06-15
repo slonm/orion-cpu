48d78b1137fffec0eab6804e1d56e8b81f7417a5
slobodyanukma <slobodyanukma@3978162c-2b8c-11df-9d00-8333cd69f872>
2010-06-15 16:52:42 +0000

    Коррекции всвязи с добавлением обработчика ошибок авторизации Багофиксы

--- trunk/cpu/cpu-license-tapestry/src/main/java/orion/cpu/views/tapestry/pages/uch/ListLicenseRecordView.java
+++ trunk/cpu/cpu-license-tapestry/src/main/java/orion/cpu/views/tapestry/pages/uch/ListLicenseRecordView.java
@@ -1,20 +1,13 @@
 package orion.cpu.views.tapestry.pages.uch;
 
-import orion.cpu.views.tapestry.pages.*;
 import orion.cpu.entities.uch.*;
-import br.com.arsmachina.authentication.entity.User;
 import br.com.arsmachina.module.service.ControllerSource;
 import br.com.arsmachina.tapestrycrud.base.BaseListPage;
 import java.util.List;
 import org.apache.tapestry5.EventContext;
-import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.ioc.annotations.Symbol;
 import orion.cpu.baseentities.BaseEntity;
-import orion.cpu.views.tapestry.pages.ErrorReport;
-import orion.cpu.views.tapestry.pages.Login;
 
 /**
  *
@@ -26,12 +19,6 @@ public class ListLicenseRecordView extends BaseListPage<LicenseRecordView, Integ
     private ControllerSource controllerSource;
     @Property
     private String license;
-    @InjectPage
-    private ErrorReport errorReport;
-    @InjectPage
-    private Login login;
-    @SessionState(create = false)
-    private User user;
 
     /**
      * При закрытии страницы
@@ -56,18 +43,8 @@ public class ListLicenseRecordView extends BaseListPage<LicenseRecordView, Integ
      */
     @SuppressWarnings("unchecked")
     public Object onActivate(EventContext context) {
-        if (!getAuthorizer().canSearch(getEntityClass())) {
-            if (user != null) {
-                return errorReport.getErrorReportLink(ErrorReport.ACCESS_DENIED);
-            } else {
-                return login.setRedirectURL();
-            }
-        }
+        getAuthorizer().checkSearch(getEntityClass());
         List<License> licList = controllerSource.get(License.class).findAll();
-        if(licList.size()==0){
-            //TODO сделать нормальное сообщение
-            return errorReport.getErrorReportLink(ErrorReport.ACCESS_DENIED);
-        }
         license = licList.get(0).toString();
         return null;
     }
