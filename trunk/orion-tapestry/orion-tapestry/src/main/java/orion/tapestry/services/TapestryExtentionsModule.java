8247a33a9a25f2dd078ce582883518d11e721278
slobodyanukma <slobodyanukma@3978162c-2b8c-11df-9d00-8333cd69f872>
2010-06-17 15:46:03 +0000

    Транслятор Byte[] в String. Для Blob полей базы данных, хранящих текст

--- trunk/orion-tapestry/orion-tapestry/src/main/java/orion/tapestry/services/TapestryExtentionsModule.java
+++ trunk/orion-tapestry/orion-tapestry/src/main/java/orion/tapestry/services/TapestryExtentionsModule.java
@@ -1,6 +1,7 @@
 package orion.tapestry.services;
 
 import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.Translator;
 import org.apache.tapestry5.internal.InternalSymbols;
 import org.apache.tapestry5.ioc.*;
 import org.apache.tapestry5.ioc.annotations.Match;
@@ -12,6 +13,7 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.annotations.Symbol;
 import org.apache.tapestry5.services.ValidationConstraintGenerator;
 import orion.tapestry.beaneditor.HibernateAnnotationsConstraintGenerator;
+import orion.tapestry.utils.ByteArrayTranslator;
 
 /**
  * Модуль конфигурации IOC
@@ -30,7 +32,7 @@ public class TapestryExtentionsModule {
         //вместо этого модифицируем работу сервиса ComponentMessagesSourceImpl
         configuration.override(SymbolConstants.APPLICATION_CATALOG, "*");
     }
-    
+
     /**
      * Регистрация блоков для автоформирования beanmodel
      * @param configuration конфигурация
@@ -75,8 +77,13 @@ public class TapestryExtentionsModule {
     }
 
     public static void contributeGlobalMessageAppender(OrderedConfiguration<String> configuration,
-            @Inject @Symbol(InternalSymbols.APP_NAME) String app_name){
+            @Inject @Symbol(InternalSymbols.APP_NAME) String app_name) {
         //Последний каталог это глобальный каталог приложения.
         configuration.add(SymbolConstants.APPLICATION_CATALOG, String.format("context:WEB-INF/%s.properties", app_name), "after:*");
+    }
+
+    public static void contributeTranslatorSource(Configuration<Translator> configuration) {
+
+        configuration.add(new ByteArrayTranslator());
     }
 }
