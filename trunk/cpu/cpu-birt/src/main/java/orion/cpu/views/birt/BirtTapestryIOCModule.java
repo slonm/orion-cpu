3909bb8a69b0c5c2bfee75452dc76ffbeaafe5bc
slobodyanukma <slobodyanukma@3978162c-2b8c-11df-9d00-8333cd69f872>
2010-06-25 08:06:37 +0000

    Доработка загрузки отчетов

--- trunk/cpu/cpu-birt/src/main/java/orion/cpu/views/birt/BirtTapestryIOCModule.java
+++ trunk/cpu/cpu-birt/src/main/java/orion/cpu/views/birt/BirtTapestryIOCModule.java
@@ -1,14 +1,10 @@
 package orion.cpu.views.birt;
 
-import br.com.arsmachina.module.service.ControllerSource;
-import java.net.URLStreamHandler;
 import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.services.LibraryMapping;
-import orion.cpu.controllers.NamedEntityController;
-import orion.cpu.entities.sys.ReportTemplate;
 
 public class BirtTapestryIOCModule {
 
@@ -22,12 +18,6 @@ public class BirtTapestryIOCModule {
         configuration.add("/preview*");
 //	      configuration.add("/output*");      
 
-    }
-
-    public static void contributeURLStreamHandlerFactory(MappedConfiguration<String, URLStreamHandler> configuration,
-            ControllerSource controllerSource) {
-        NamedEntityController<ReportTemplate> controller = (NamedEntityController<ReportTemplate>) (Object) controllerSource.get(ReportTemplate.class);
-        configuration.add("rptdesign", new RptDesignURLStreamHandler(controller));
     }
 
     public static void contributeRegistryStartup(OrderedConfiguration<Runnable> configuration) {
@@ -45,5 +35,11 @@ public class BirtTapestryIOCModule {
     public static void contributeComponentClassResolver(Configuration<LibraryMapping> configuration) {
         configuration.add(new LibraryMapping("birt", "orion.cpu.views.birt"));
     }
+
+    //FIXME Этот сервис объявлен в cpu-tapestry и вводит зависимость от этой библиотеки
+    public static void contributeDataURLStreamHandler(MappedConfiguration<String, Class<?>> configuration){
+        configuration.add("rptdesign", DataRptDesignURLConnection.class);
+    }
+
 
 }
