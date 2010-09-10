cda8c0ffd3ee47d07fcc0574ce1a73c7d30fb8e8
slobodyanukma <slobodyanukma@3978162c-2b8c-11df-9d00-8333cd69f872>
2010-09-10 12:13:20 +0000

    clean and fix

--- orion-tapestry/cpu-menu/src/main/java/orion/tapestry/menu/services/CpuMenuModule.java
+++ orion-tapestry/cpu-menu/src/main/java/orion/tapestry/menu/services/CpuMenuModule.java
@@ -5,17 +5,13 @@
 package orion.tapestry.menu.services;
 
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.annotations.ServiceId;
 import org.apache.tapestry5.ioc.services.SymbolSource;
 import org.apache.tapestry5.services.LibraryMapping;
-import orion.tapestry.menu.lib.DefaultMenuLink;
 import orion.tapestry.menu.lib.IMenuLink;
-import orion.tapestry.menu.pages.Navigator;
 
 /**
  *
@@ -31,16 +27,6 @@ public class CpuMenuModule {
      */
     @ServiceId("CpuMenu")
     public static CpuMenu buildCpuMenu(Map<String, IMenuLink> configuration,  @Inject SymbolSource ss ) {
-        String navigatorPageClass = ss.valueForSymbol("cpumenu.navigatorpage");
-        Logger logger = LoggerFactory.getLogger(CpuMenuModule.class);
-        logger.info("buildCpuMenu::cpu-menu-navigator-page="+navigatorPageClass);
-        try {
-            DefaultMenuLink.setNavigatorPageClass(Class.forName(navigatorPageClass));
-            //DefaultMenuLink.setNavigatorPageClass(Class.forName(Navigator.class.getName()));
-        } catch (ClassNotFoundException ex) {
-            //Logger logger = LoggerFactory.getLogger(CpuMenuModule.class);
-            logger.info(navigatorPageClass + " - class not found ");
-        }
         return new CpuMenuImpl(configuration);
     }
 
@@ -52,9 +38,4 @@ public class CpuMenuModule {
         configuration.add("menu/1.0", "orion/tapestry/menu");
     }
 
-    public void contributeFactoryDefaults(MappedConfiguration<String, String> configuration) {
-        //Logger logger = LoggerFactory.getLogger(CpuMenuModule.class);
-        //logger.info("contributeFactoryDefaults::cpumenu.navigatorpage="+Navigator.class.getName());
-        configuration.add("cpumenu.navigatorpage", Navigator.class.getName());
-    }
 }
