ca98ea9ed153b811c5076d67e9ae11119b67307c
slobodyanukma <slobodyanukma@3978162c-2b8c-11df-9d00-8333cd69f872>
2010-11-26 10:43:28 +0000

    fixes, and reorganizes

--- cpu/cpu-units/src/main/java/orion/cpu/entities/org/OrgUnit.java
+++ cpu/cpu-units/src/main/java/orion/cpu/entities/org/OrgUnit.java
@@ -1,31 +1,43 @@
 package orion.cpu.entities.org;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import orion.cpu.baseentities.NamedEntity;
 
 /**
- * Базовое абстрактное структурное подразделение.
- * Содержит поля базовой таблицы Reference
+ * Базовое абстрактная структурная единица.
  * @author sl
  */
 @Entity
-@Table(schema = "org", uniqueConstraints =
-@UniqueConstraint(columnNames = "name"))
+@Table(schema = "org", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "name"),
+    @UniqueConstraint(columnNames = "shortName")
+})
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class OrgUnit extends NamedEntity<OrgUnit> {
 
     private static final long serialVersionUID = 1L;
-    @Size(min = 1)
+    private OrgUnit parent;
     private String shortName;
 
-    @Column(unique = true)
+    @Size(min = 1)
+    @NotNull
     public String getShortName() {
         return shortName;
     }
 
     public void setShortName(String shortName) {
         this.shortName = shortName;
+    }
+
+    @ManyToOne
+    public OrgUnit getParent() {
+        return parent;
+    }
+
+    public void setParent(OrgUnit parent) {
+        this.parent = parent;
     }
 
     @Override
