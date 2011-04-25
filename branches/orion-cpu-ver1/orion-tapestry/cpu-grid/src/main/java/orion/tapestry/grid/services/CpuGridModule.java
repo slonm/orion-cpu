368b77d6cfcc812c12c3f223125d86b0f837b734
gen.dobr <gen.dobr@3978162c-2b8c-11df-9d00-8333cd69f872>
2011-04-25 21:11:25 +0000

    -

--- branches/orion-cpu-ver1/orion-tapestry/cpu-grid/src/main/java/orion/tapestry/grid/services/CpuGridModule.java
+++ branches/orion-cpu-ver1/orion-tapestry/cpu-grid/src/main/java/orion/tapestry/grid/services/CpuGridModule.java
@@ -1,12 +1,12 @@
 package orion.tapestry.grid.services;
 
-
 import java.util.Map;
 import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.annotations.ServiceId;
 import org.apache.tapestry5.services.LibraryMapping;
 import orion.tapestry.grid.lib.field.GridFieldAbstract;
+import orion.tapestry.grid.lib.field.impl.GridFieldBoolean;
 import orion.tapestry.grid.lib.field.impl.GridFieldDate;
 import orion.tapestry.grid.lib.field.impl.GridFieldNumberByte;
 import orion.tapestry.grid.lib.field.impl.GridFieldNumberDouble;
@@ -20,6 +20,7 @@ import orion.tapestry.grid.lib.field.impl.GridFieldString;
  * @author Gennadiy Dobrovolsky
  */
 public class CpuGridModule {
+
     public static void contributeComponentClassResolver(Configuration<LibraryMapping> configuration) {
         configuration.add(new LibraryMapping("grid", "orion.tapestry.grid"));
     }
@@ -28,11 +29,6 @@ public class CpuGridModule {
         configuration.add("grid-1.0", "orion.tapestry.grid");
     }
 
-
-
-
-
-    
     /**
      * Создаёт сервис-контейнер,
      * хранящий соответствие между типом данных в Java
@@ -45,34 +41,35 @@ public class CpuGridModule {
         return new GridTypeMapImpl(configuration);
     }
 
-
-
     /**
      * Конфигурация по умолчанию, соответствие между типом данных в Java и типом колонки в Grid
      * Add map Class => GridField type
      * @param configuration соответствие между типом данных в Java и типом колонки в Grid
      */
-    public static void contributeTypeMap(MappedConfiguration<String ,Class<? extends GridFieldAbstract> > configuration) {
-        configuration.add("java.lang.String" , GridFieldString.class);
+    public static void contributeTypeMap(MappedConfiguration<String, Class<? extends GridFieldAbstract>> configuration) {
+        configuration.add("java.lang.String", GridFieldString.class);
 
-        configuration.add("byte"             , GridFieldNumberByte.class);
-        configuration.add("java.lang.Byte"   , GridFieldNumberByte.class);
+        configuration.add("byte", GridFieldNumberByte.class);
+        configuration.add("java.lang.Byte", GridFieldNumberByte.class);
 
-        configuration.add("short"            , GridFieldNumberShort.class);
-        configuration.add("java.lang.Short"  , GridFieldNumberShort.class);
+        configuration.add("short", GridFieldNumberShort.class);
+        configuration.add("java.lang.Short", GridFieldNumberShort.class);
 
-        configuration.add("int"              , GridFieldNumberInteger.class);
+        configuration.add("int", GridFieldNumberInteger.class);
         configuration.add("java.lang.Integer", GridFieldNumberInteger.class);
 
-        configuration.add("long"             , GridFieldNumberLong.class);
-        configuration.add("java.lang.Long"   , GridFieldNumberLong.class);
+        configuration.add("long", GridFieldNumberLong.class);
+        configuration.add("java.lang.Long", GridFieldNumberLong.class);
 
-        configuration.add("float"            , GridFieldNumberFloat.class);
-        configuration.add("java.lang.Float"  , GridFieldNumberFloat.class);
+        configuration.add("float", GridFieldNumberFloat.class);
+        configuration.add("java.lang.Float", GridFieldNumberFloat.class);
 
-        configuration.add("double"           , GridFieldNumberDouble.class);
-        configuration.add("java.lang.Double" , GridFieldNumberDouble.class);
+        configuration.add("double", GridFieldNumberDouble.class);
+        configuration.add("java.lang.Double", GridFieldNumberDouble.class);
 
-        configuration.add("java.util.Date"   , GridFieldDate.class);
+        configuration.add("java.util.Date", GridFieldDate.class);
+
+        configuration.add("boolean", GridFieldBoolean.class);
+        configuration.add("java.lang.Boolean", GridFieldBoolean.class);
     }
 }
