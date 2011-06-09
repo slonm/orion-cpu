1b93b328116e4cbdd8f0a6e4ca946847ae09d6d3
kgp@mail.zp.ua <kgp@mail.zp.ua@3978162c-2b8c-11df-9d00-8333cd69f872>
2011-06-09 07:54:39 +0000

    Создаю приватную копию branches/kgp/cpu-v2 из /branches/sl/version2 (r431)

---
+++ branches/kgp/cpu-v2/cpu/src/main/java/ua/orion/cpu/web/services/AppModule.java
@@ -0,0 +1,64 @@
+package ua.orion.cpu.web.services;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.ioc.*;
+import org.apache.tapestry5.ioc.annotations.Match;
+import org.apache.tapestry5.ioc.annotations.Startup;
+import org.apache.tapestry5.ioc.annotations.SubModule;
+import org.apache.tapestry5.ioc.annotations.Symbol;
+import org.apache.tapestry5.services.ComponentClassResolver;
+import org.apache.tapestry5.services.linktransform.PageRenderLinkTransformer;
+import org.tynamo.jpa.Ejb3HibernateSymbols;
+import ua.orion.core.utils.IOCUtils;
+import ua.orion.cpu.core.OrionCPUSymbols;
+import ua.orion.cpu.core.licensing.services.LicensingIOCModule;
+import ua.orion.cpu.core.orgunits.services.OrgUnitsIOCModule;
+import ua.orion.cpu.core.security.services.OrionSecurityIOCModule;
+import ua.orion.cpu.core.services.OrionCpuIOCModule;
+import ua.orion.cpu.web.licensing.pages.LicensingWebIOCModule;
+import ua.orion.web.security.services.OrionSecurityWebIOCModule;
+
+/**
+ * Модуль конфигурирования IOC
+ */
+@SubModule({
+    LicensingIOCModule.class,
+    OrionSecurityIOCModule.class,
+    OrgUnitsIOCModule.class,
+    OrionCpuIOCModule.class,
+    LicensingWebIOCModule.class,
+    OrionSecurityWebIOCModule.class
+})
+public class AppModule {
+
+    public static void bind(ServiceBinder binder) {
+        binder.bind(IndexPageRenderLinkTransformer.class);
+    }
+
+    /**
+     * Регистрация и конфигурирование опций.
+     * @param configuration
+     */
+    public static void contributeApplicationDefaults(
+            MappedConfiguration<String, String> configuration) {
+        ResourceBundle bundle = ResourceBundle.getBundle("Cpu");
+        Enumeration<String> e = bundle.getKeys();
+        while (e.hasMoreElements()) {
+            String key = e.nextElement();
+            configuration.add(key, bundle.getString(key));
+        }
+    }
+
+//    public static void contributeComponentEventLinkTransformer(
+//            OrderedConfiguration<ComponentEventLinkTransformer> configuration)
+//    {
+//        configuration.addInstance("App", AppComponentEventLinkTransformer.class);
+//    }
+//    public static void contributeRealmSource(Configuration<Realm> configuration) {
+//        ExtendedPropertiesRealm realm = new ExtendedPropertiesRealm("classpath:shiro-users.properties");
+//        configuration.add(realm);
+//    }
+    
+}
---
+++ branches/kgp/cpu-v2/orion-core/src/main/java/ua/orion/cpu/core/orgunits/entities/Institute.java
@@ -0,0 +1,20 @@
+package ua.orion.cpu.core.orgunits.entities;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import javax.persistence.*;
+
+/**
+ * Класс-сущность институт
+ * @author kgp
+ */
+@Entity
+@Table(schema = "org")
+public class Institute extends OrgUnit<Institute> {
+
+   private static final long serialVersionUID = 1L;
+}
