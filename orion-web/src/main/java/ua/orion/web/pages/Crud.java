de0c52587d11048e7fa1351f94c346268314a500
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2011-07-03 12:50:05 +0000

    Функциональность CRUD перенесена в самостоятельный компонент

--- orion-web/src/main/java/ua/orion/web/pages/Crud.java
+++ orion-web/src/main/java/ua/orion/web/pages/Crud.java
@@ -1,13 +1,10 @@
 package ua.orion.web.pages;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.tapestry5.Block;
 import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.EventContext;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.annotations.*;
-import org.apache.tapestry5.corelib.components.Zone;
-import org.apache.tapestry5.grid.GridDataSource;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.annotations.Symbol;
@@ -15,29 +12,16 @@ import org.apache.tapestry5.services.ComponentEventLinkEncoder;
 import org.apache.tapestry5.services.Request;
 import org.slf4j.Logger;
 import ua.orion.core.persistence.IEntity;
-import ua.orion.core.services.EntityService;
 import ua.orion.core.persistence.MetaEntity;
 import ua.orion.web.services.LastPageHolder;
-import ua.orion.web.services.TapestryDataSource;
 
 /**
  * Страница, которая предоставляет CRUD для простых сущностей
  * @author slobodyanuk
  */
-@Import(library = "../WindowUtils.js",
-stylesheet = "../css/tapestry-crud.css")
 @SuppressWarnings("unused")
 public class Crud {
     //---Components and component's resources---
-
-    @Component
-    private Zone listZone;
-    @Inject
-    private Block editBlock;
-    @Inject
-    private Block viewBlock;
-    @Inject
-    private Block deleteBlock;
     @Inject
     @Property(write = false)
     private Messages messages;
@@ -49,12 +33,7 @@ public class Crud {
     @Inject
     private LastPageHolder lastPageHolder;
     @Inject
-    private EntityService entityService;
-    @Inject
     private Logger LOG;
-    @Inject
-    @Property(write = false)
-    private TapestryDataSource dataSource;
     @Inject
     private ComponentEventLinkEncoder componentEventLinkEncoder;
     @Inject
@@ -63,36 +42,13 @@ public class Crud {
     //---Locals---
     @Persist
     private Class<? extends IEntity> objectClass;
-    @Persist
-    private IEntity object;
-    @Persist
-    @Property(write = false)
-    private String mode;
-    @Property(write = false)
-    private String title;
-    @Property(write = false)
-    private String error;
-    private static final String EDIT = "edit";
-    private static final String ADD = "add";
-    private static final String VIEW = "view";
-    private static final String DEL = "del";
 
     public boolean isComponentEventRequst() {
         return componentEventLinkEncoder.decodeComponentEventRequest(request) != null;
     }
 
-    /**
-     * Задано явно для возможности вызова из других классов
-     */
-    public IEntity getObject() {
-        return object;
-    }
-
-    /**
-     * Задано явно для возможности вызова из других классов
-     */
-    public void setObject(IEntity object) {
-        this.object = object;
+    public String getTitle() {
+        return messages.get("entity." + objectClass.getSimpleName());
     }
 
     /**
@@ -109,20 +65,6 @@ public class Crud {
         this.objectClass = objectClass;
     }
 
-    /**
-     * При закрытии страницы
-     * @return
-     * @author sl
-     */
-//    public void onPassivate() {
-//        if (getObject() != null) {
-//            return getActivationContextEncoder(getEntityClass()).
-//                    toActivationContext(getObject());
-//        } else if (isInitiated()) {
-//            return BaseEntity.getFullClassName(getEntityClass());
-//        }
-//        return null;
-//    }
     public Object onActivate(EventContext context) {
         if (!isComponentEventRequst()) {
             try {
@@ -144,77 +86,6 @@ public class Crud {
             }
         }
         SecurityUtils.getSubject().checkPermission(objectClass.getSimpleName() + ":read");
-        title = messages.get("entity." + objectClass.getSimpleName());
         return null;
-    }
-
-    public GridDataSource getObjects() {
-        return dataSource.getGridDataSource(objectClass);
-    }
-
-    public Object onSuccessFromEditForm() {
-        error = null;
-        try {
-            entityService.merge(object);
-        } catch (RuntimeException ex) {
-            error = messages.get("message.update-error");
-        }
-        return listZone.getBody();
-    }
-
-    public Object onSuccessFromAddForm() {
-        error = null;
-        try {
-            entityService.persist(object);
-        } catch (RuntimeException ex) {
-            error = messages.get("message.insert-error");
-        }
-        return listZone.getBody();
-    }
-
-    public Object onEdit(Integer id) {
-        SecurityUtils.getSubject().checkPermission(objectClass.getSimpleName() + ":update:" + id);
-        mode = EDIT;
-        object = entityService.find(objectClass, id);
-        error = null;
-        return editBlock;
-    }
-
-    public Object onAdd() {
-        SecurityUtils.getSubject().checkPermission(objectClass.getSimpleName() + ":insert");
-        mode = ADD;
-        object = entityService.newInstance(objectClass);
-        error = null;
-        return editBlock;
-    }
-
-    public Object onView(Integer id) {
-        mode = VIEW;
-        object = entityService.find(objectClass, id);
-        error = null;
-        return viewBlock;
-    }
-
-    public Object onTryDelete(Integer id) {
-        SecurityUtils.getSubject().checkPermission(objectClass.getSimpleName() + ":delete:" + id);
-        mode = DEL;
-        object = entityService.find(objectClass, id);
-        error = null;
-        return deleteBlock;
-    }
-
-    public Object onDelete(Integer id) {
-        error = null;
-        try {
-            object = entityService.find(objectClass, id);
-            entityService.remove(object);
-        } catch (RuntimeException ex) {
-            error = messages.get("message.delete-error");
-        }
-        return listZone.getBody();
-    }
-
-    public boolean getIsEdit() {
-        return EDIT.equals(mode);
     }
 }
