0f094ecde988262559a741cd1057a63402e7ffdf
gen.dobr <gen.dobr@3978162c-2b8c-11df-9d00-8333cd69f872>
2011-07-10 19:22:03 +0000

    testing jpa grid model

---
+++ branches/dobro/cpu-grid-jpa-test/src/main/java/test/tapestry/services/TstModule.java
@@ -0,0 +1,21 @@
+package test.tapestry.services;
+
+import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.annotations.SubModule;
+import org.tynamo.jpa.JPASymbols;
+
+/**
+ * This module is automatically included as part of the Tapestry IoC Registry, it's a good place to
+ * configure and extend Tapestry, or to place your own service definitions.
+ */
+//Перечень дополнительных модулей
+@SubModule({orion.tapestry.grid.services.CpuGridModule.class})
+public class TstModule {
+
+    public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration) {
+        configuration.add("tapestry.production-mode", "false");
+
+      configuration.add(JPASymbols.PERSISTENCE_UNIT, "sample");
+
+    }
+}
