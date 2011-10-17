b2e19410d807bc8979b40806211e54c4fbbd09bf
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2011-10-17 12:25:59 +0000

    Убраны "лишние" роли в списке выбора роли

--- trunk/orion-web/src/main/java/ua/orion/web/pages/SelectRole.java
+++ trunk/orion-web/src/main/java/ua/orion/web/pages/SelectRole.java
@@ -15,48 +15,50 @@ import ua.orion.cpu.core.security.entities.ActiveDirectoryPrincipal;
 import ua.orion.cpu.core.security.services.ThreadRole;
 
 public class SelectRole {
-
+    
     @Inject
     private ThreadRole thRole;
-
+    
     public String getRole() {
         return thRole.getRole();
     }
-
+    
     public void setRole(String role) {
         thRole.setRole(role);
     }
-
+    
     public SelectModel getRoleModel() {
         return new AbstractSelectModel() {
-
+            
             @Override
             public List<OptionGroupModel> getOptionGroups() {
                 return null;
             }
-
+            
             @Override
             public List<OptionModel> getOptions() {
                 List<OptionModel> optionModelList = new ArrayList<OptionModel>();
                 PrincipalCollection pc = SecurityUtils.getSubject().getPrincipals();
-
+                
                 if (pc != null) {
                     ActiveDirectoryPrincipal adp = pc.oneByType(ActiveDirectoryPrincipal.class);
                     if (adp != null) {
                         Set<String> roles = adp.getRoles();
                         for (final String _role : roles) {
-                            optionModelList.add(new AbstractOptionModel() {
-
-                                @Override
-                                public String getLabel() {
-                                    return _role;
-                                }
-
-                                @Override
-                                public Object getValue() {
-                                    return _role;
-                                }
-                            });
+                            if (_role.startsWith("kis/")) {
+                                optionModelList.add(new AbstractOptionModel() {
+                                    
+                                    @Override
+                                    public String getLabel() {
+                                        return _role;
+                                    }
+                                    
+                                    @Override
+                                    public Object getValue() {
+                                        return _role;
+                                    }
+                                });
+                            }
                         }
                     }
                 }
@@ -64,7 +66,7 @@ public class SelectRole {
             }
         };
     }
-
+    
     public Object onActionFromRoleForm() {
         return "";
     }
