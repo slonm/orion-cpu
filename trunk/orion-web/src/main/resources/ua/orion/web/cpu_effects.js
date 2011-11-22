b169b4c07bf30ad89593617755275e7b1b737332
DVelopp@gmail.com <DVelopp@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2011-11-22 10:22:39 +0000

    New functionality of grid! There are no actioncell column. All actions on the popup window, which show where mouseenter event invoked on the table row. There are new window for ajax-state with modal state.

--- trunk/orion-web/src/main/resources/ua/orion/web/cpu_effects.js
+++ trunk/orion-web/src/main/resources/ua/orion/web/cpu_effects.js
@@ -9,6 +9,20 @@ jQuery(document).ready(function(){
     if (jQuery("ui#interface").text()=="true"){
         initializeUIComponents();   
     }
+    //Инициализация подсказок
+    initializeGridToolTip();
+    //Инициализация окна для отображения загрузки Ajax
+    jQuery('#ui-ajax-loading-window').dialog({
+        modal:true, //модальность
+        autoOpen: false, //автоматическое открытие
+        open: function() { 
+            jQuery(".ui-dialog-titlebar-close").hide(); //убирается кнопка закрытия
+        },
+        resizable: false //возможность маштабирования отключена
+    });
+        jQuery("#crud-add-record-button").click(function(){
+        Tapestry.Initializer.showGridAjax(); //При добавлении записи появляется окно загрузки
+    })
 })
 
 
@@ -26,4 +40,31 @@ function createToolTips(){
     jQuery("select").easyTooltip({
         ctype:"select"
     });
+}
+
+
+/**
+ * Появление подсказки над строками Grid с кнопками actioncell. 
+ */
+function initializeGridToolTip(){
+    //При наведении на строку появляется tooltip
+    jQuery("table.t-data-grid tbody tr").each(function(){
+        jQuery(this).tooltip({
+            effect: 'slide', //Тип эффекта
+            delay: '150', //Время исчезновения при отведении мыши
+            tip: jQuery(this).find(".ui-grid-cell-action-tip"), //Элемент с подсказкой
+            position: 'bottom center' //Позиция
+        });
+    })   
+    //Настройки изменения стилей при наведении и отведении мыши
+    jQuery(".ui-grid-cell-action-tip").css({
+        'width':jQuery("table.t-data-grid").css("width")-2,
+        'margin-left':jQuery("table.t-data-grid").css("margin-left")
+        });
+    jQuery("table.t-data-grid tbody tr td[class!='action']").bind("mouseenter", function(){
+        jQuery(this).parent().find("td[class!='action']").addClass("ui-state-highlight").removeClass("ui-widget-content");
+    });
+    jQuery("table.t-data-grid tbody tr td[class!='action']").bind("mouseleave", function(){
+        jQuery(this).parent().find("td[class!='action']").addClass("ui-widget-content").removeClass("ui-state-highlight");
+    });	
 }
