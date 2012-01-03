5a11e466f6825003d4b108ec8f36fb2f310435f6
kgp@mail.zp.ua <kgp@mail.zp.ua@3978162c-2b8c-11df-9d00-8333cd69f872>
2012-01-03 17:10:45 +0000

    Delete wrong import

--- branches/orion-cpu-ver1/cpu/cpu-core/src/main/java/orion/cpu/controllers/listenersupport/AbortControllerEventException.java
+++
@@ -1,9 +0,0 @@
-package orion.cpu.controllers.listenersupport;
-
-/**
- * Отмена операции.
- * При генерации этого исключения оно тихо обрабатывается контроллером.
- * @author sl
- */
-public class AbortControllerEventException extends RuntimeException {
-}
