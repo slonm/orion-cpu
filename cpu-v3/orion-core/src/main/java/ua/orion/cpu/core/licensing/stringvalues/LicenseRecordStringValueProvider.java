fcf2ab5a6c870dd75db91adf194920ef08d858fc
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2012-01-25 12:11:17 +0000

    StringValueProvider теперь более общий и кроме обработки сущностей работает так-же с любыми другими типами. В частности преобразовывает в строку даты (Calendar и Date).

--- cpu-v3/orion-core/src/main/java/ua/orion/cpu/core/licensing/stringvalues/LicenseRecordStringValueProvider.java
+++ cpu-v3/orion-core/src/main/java/ua/orion/cpu/core/licensing/stringvalues/LicenseRecordStringValueProvider.java
@@ -1,8 +1,6 @@
 package ua.orion.cpu.core.licensing.stringvalues;
 
-import java.text.DateFormat;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.ioc.services.ThreadLocale;
 import ua.orion.core.services.StringValueProvider;
 import ua.orion.cpu.core.licensing.entities.LicenseRecord;
 
@@ -13,16 +11,18 @@ import ua.orion.cpu.core.licensing.entities.LicenseRecord;
 public class LicenseRecordStringValueProvider implements StringValueProvider<LicenseRecord> {
 
     @Inject
-    private ThreadLocale thLocale;
+    private StringValueProvider stringValueProvider;
 
     @Override
     public String getStringValue(LicenseRecord e) {
-        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, thLocale.getLocale());
+        if (e == null) {
+            return null;
+        }
         StringBuilder sb = new StringBuilder();
         sb.append(e.getCode()).append(" - ");
         sb.append(e.getSpeciality() == null ? e.getTrainingDirection().getName() : e.getSpeciality().getName()).append(" (");
         sb.append(e.getLicenseRecordGroup().getName()).append(") ");
-        sb.append(dateFormat.format(e.getTermination().getTime()));
+//        sb.append(stringValueProvider.getStringValue(e.getTermination()));
         return sb.toString();
     }
 }
