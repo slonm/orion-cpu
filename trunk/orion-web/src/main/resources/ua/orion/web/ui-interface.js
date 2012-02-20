d77352f39bcd2789fd4d91b2a61e53c8a11dad9e
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2012-02-20 15:15:46 +0000

    Создан компонент для переключения языков интерфейса. Изменен вид всплывающих кнопок Crud Некоторая реорганизация стилей

--- trunk/orion-web/src/main/resources/ua/orion/web/ui-interface.js
+++ trunk/orion-web/src/main/resources/ua/orion/web/ui-interface.js
@@ -1,8 +1,15 @@
 //Инициализация классов кнопок, к которым присоеденяются иконки
+//TODO Почему бы не сделать это в виде ассоциативного массива? Пример:
+//var elements={"ui-button-close":"ui-icon-close",
+//              "ui-button-add":"ui-icon-circle-plus"}
 //tfalse в конце используется для того, чтобы сделать кнопку без текста. 
-var elements = new Array("ui-button-close","ui-button-add","ui-button-refresh","ui-button-login","ui-button-quit","ui-button-del","ui-button-cancel","ui-button-edit-tfalse","ui-button-view-tfalse","ui-button-del-tfalse","ui-button-tolist","ui-button-right-list-tfalse");
+//var elements = new Array("ui-button-close","ui-button-add","ui-button-refresh","ui-button-login","ui-button-quit","ui-button-del","ui-button-cancel","ui-button-edit-tfalse","ui-button-view-tfalse","ui-button-del-tfalse","ui-button-tolist","ui-button-right-list-tfalse");
+var elements = new Array("ui-button-close","ui-button-add","ui-button-refresh","ui-button-login","ui-button-quit","ui-button-del","ui-button-cancel","ui-button-edit-tfalse","ui-button-view-tfalse","ui-button-del-tfalse","ui-button-tolist","ui-button-right-list-tfalse",
+"ui-button-detail", "ui-button-edit","ui-button-view","ui-button-del");
 //Инициализация классов иконок, для кнопок в массиве elements
-var icons = new Array("ui-icon-close","ui-icon-circle-plus","ui-icon-refresh","ui-icon-key","ui-icon-power","ui-icon-trash", "ui-icon-cancel", "ui-icon-pencil","ui-icon-print","ui-icon-trash","ui-icon-arrowreturnthick-1-n","ui-icon-triangle-1-e");
+//var icons = new Array("ui-icon-close","ui-icon-circle-plus","ui-icon-refresh","ui-icon-key","ui-icon-power","ui-icon-trash", "ui-icon-cancel", "ui-icon-pencil","ui-icon-print","ui-icon-trash","ui-icon-arrowreturnthick-1-n","ui-icon-triangle-1-e");
+var icons = new Array("ui-icon-close","ui-icon-circle-plus","ui-icon-refresh","ui-icon-key","ui-icon-power","ui-icon-trash", "ui-icon-cancel", "ui-icon-pencil","ui-icon-print","ui-icon-trash","ui-icon-arrowreturnthick-1-n","ui-icon-triangle-1-e", 
+"ui-icon-zoomin", "ui-icon-pencil", "ui-icon-search", "ui-icon-circle-minus");
     
 //Инициализация иконок на кнопках
 //ec - класс элемента HTML, ic - класс иконик (из jquery-ui)
@@ -68,6 +75,7 @@ function initializeUIComponents(){
     //Создание выпадающего списка из select
     ///jQuery("select").selectmenu();
     initializeCheckboxes();
+    jQuery(".ui-buttonset").buttonset();
     jQuery("button").parent('a').css({
         'text-decoration':'none'
     })
@@ -168,11 +176,11 @@ function applyCookieTheme(){
  * Добавление UI-классов к элементам интерфейса
  */
 function updateCSS(){
-    jQuery("table.t-data-grid tbody tr td[class!='action']").addClass("ui-widget-content");
+    //jQuery("table.t-data-grid tbody tr td[class!='action']").addClass("ui-widget-content");
     jQuery("table.t-data-grid thead tr th[class!='action t-last']").addClass("ui-state-default");
     jQuery("div.t-data-grid-pager a").addClass("ui-corner-all ui-state-default");
     jQuery("div.t-data-grid-pager span.current").addClass("ui-corner-all ui-state-active");
-    jQuery(".ui-grid-cell-action-tip").addClass("ui-corner-tr ui-corner-br ui-state-default");
+    jQuery(".ui-grid-cell-action-tip").addClass("ui-corner-all ui-state-default");
     initializeUIComponents();
 }
 
