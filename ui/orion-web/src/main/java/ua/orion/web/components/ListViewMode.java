05b208b45915c6313494b3521884c12af1993017
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2012-02-21 19:09:58 +0000

    Begin ListView

---
+++ ui/orion-web/src/main/java/ua/orion/web/components/ListViewMode.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.orion.web.components;
+
+/**
+ *
+ * @author slobodyanuk
+ */
+public enum ListViewMode {
+
+    /**
+     * Uses large icons
+     */
+    TILES,
+    /**
+     * A simple list
+     */
+    LIST,
+    /**
+     * A list that includes various properties. Useful for sorting
+     */
+    DETAILS;
+}
