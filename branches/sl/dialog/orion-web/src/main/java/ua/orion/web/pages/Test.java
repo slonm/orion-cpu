a822b2ff214c516fe8b7f78420dc9d84b8904b02
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2012-03-06 15:11:12 +0000

    Бекап

---
+++ branches/sl/dialog/orion-web/src/main/java/ua/orion/web/pages/Test.java
@@ -0,0 +1,44 @@
+package ua.orion.web.pages;
+
+import javax.inject.Inject;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
+import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
+import org.apache.tapestry5.services.ajax.JavaScriptCallback;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+
+public class Test {
+
+    @Inject
+    private Block content;
+    @Inject
+    private Block econtent;
+    @Inject
+    private AjaxResponseRenderer ajaxResponseRenderer;
+    @Property
+    private String title;
+
+    Object onShow() {
+        ajaxResponseRenderer.addCallback(new JavaScriptCallback() {
+
+            @Override
+            public void run(JavaScriptSupport js) {
+
+                js.addScript("jQuery('#win').dialog('open')");
+            }
+        });
+        return content;
+    }
+
+    Object onActionFromOtherEShow() {
+        title="OtherContent";
+        return econtent;
+    }
+    
+    Object onActionFromEShow() {
+        title="econtent";
+        return econtent;
+    }
+}
