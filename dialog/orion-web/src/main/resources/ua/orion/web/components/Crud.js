dd6d2ffa1358014bae5c7f633c6e9070e28133a9
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2012-03-12 20:13:01 +0000

    Бекап

--- dialog/orion-web/src/main/resources/ua/orion/web/components/Crud.js
+++ dialog/orion-web/src/main/resources/ua/orion/web/components/Crud.js
@@ -13,19 +13,21 @@ Ori.crudLoadPopupZone = function(){
 
 Ori.crudLoadListZone = function(){
     //Добавим кнопку, при наведении на которую появится tooltip
-    //с кнопками действий 
-    $J(".ui-grid-cell-action-tip").each(function(){
-        $J(this).css("display", "none");
-        $J("<button class='ui-button-right-list-tfalse'/>").appendTo($J(this).parent())
-        .tooltip({
-            effect: 'fade', //Тип эффекта
-            delay: '150', //Время исчезновения при отведении мыши
-            tip: $J(this), //Элемент с подсказкой
-            position: 'center right' //Позиция
-        });
-    })   
+    //с кнопками действий
+    var panel=$J(".ui-grid-cell-action-tip");
+    panel.css("display", "none");
+    $J("<button class='ui-button-right-list-tfalse'/>").appendTo(panel.parent())
+    .tooltip({
+        effect: 'fade', //Тип эффекта
+        delay: '150', //Время исчезновения при отведении мыши
+        tip: panel, //Элемент с подсказкой
+        position: 'center right' //Позиция
+    });
+    $J(".ui-grid-cell-action-tip a").bind("click", function(){
+        panel.hide();
+    });
     //Настройки изменения стилей при наведении и отведении мыши
-    $J(".ui-grid-cell-action-tip").css({
+    panel.css({
         'width':$J("table.t-data-grid").css("width")-2,
         'margin-left':$J("table.t-data-grid").css("margin-left")
     });
