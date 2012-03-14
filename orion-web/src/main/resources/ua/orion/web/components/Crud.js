9aef02d074a768172806e08a8869667a63bf79f5
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2012-03-14 08:58:24 +0000

    Bug Fix

--- orion-web/src/main/resources/ua/orion/web/components/Crud.js
+++ orion-web/src/main/resources/ua/orion/web/components/Crud.js
@@ -16,20 +16,16 @@ Ori.crudLoadListZone = function(){
     //с кнопками действий
     var panel=$J(".ui-grid-cell-action-tip");
     panel.css("display", "none");
-    $J("<button class='ui-button-right-list-tfalse'/>").appendTo(panel.parent())
-    .tooltip({
-        effect: 'fade', //Тип эффекта
-        delay: '150', //Время исчезновения при отведении мыши
-        tip: panel, //Элемент с подсказкой
-        position: 'center right' //Позиция
+    panel.each(function(){
+        $J("<button class='ui-button-right-list-tfalse'/>").appendTo($J(this).parent())
+        .tooltip({
+            effect: 'fade', //Тип эффекта
+            delay: '150', //Время исчезновения при отведении мыши
+            tip: $J(this), //Элемент с подсказкой
+            position: 'center right' //Позиция
+        });
     });
     $J(".ui-grid-cell-action-tip a").bind("click", function(){
         panel.hide();
     });
-    //Настройки изменения стилей при наведении и отведении мыши
-    panel.css({
-        'width':$J("table.t-data-grid").css("width")-2,
-        'margin-left':$J("table.t-data-grid").css("margin-left")
-    });
-    $J("table.t-data-grid").wrapAll("<div id=\"grid-container\"></div>");
 }
