891b2fa38ced22f39c2a9fda8b534fa5ca591873
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2012-03-15 08:20:51 +0000

    Добавлена локализация сообщений валидаторов и сообщений T5

---
+++ trunk/orion-web/src/main/resources/org/apache/tapestry5/tapestry-messages_uk.js
@@ -0,0 +1,28 @@
+Tapestry.Messages = {
+
+    pageIsLoading : "Очікуйте закінчення завантаження сторінки ...",
+
+    missingInitializer : "Function Tapestry.Initializer.#{name}() does not exist.",
+
+    missingValidator :      "Function Tapestry.Validator.#{name}() does not exist for field '#{fieldName}'.",
+
+    ajaxFailure : "Ajax failure: Status #{status} for #{request.url}: ",
+
+    ajaxRequestUnsuccessful : "Запит до серверу не вдався. Можливо це проблеми з доступом до серверу.",
+
+    clientException :     "Client exception processing response: ",
+
+    missingZone :   "Unable to locate Ajax Zone '#{id}' for dynamic update.",
+
+    noZoneManager :   "Element '#{id}' does not have an associated Tapestry.ZoneManager object." ,
+
+    pathDoesNotStartWithSlash : "External path #{path} does not start with a leading slash.",
+
+    notAnInteger : "Не ціле число",
+
+    invalidCharacter : "Невірний символ",
+
+    communicationFailed : "Зв'язок з сервером втрачено: ",
+    
+    invocationException : "Exception invoking function #{fname} with parameters #{params}: #{exception}"
+};
