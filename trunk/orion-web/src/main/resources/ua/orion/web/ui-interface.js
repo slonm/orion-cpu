08ceb8d597721b78d349e70b360853c62b7cf189
slobodyanukma@gmail.com <slobodyanukma@gmail.com@3978162c-2b8c-11df-9d00-8333cd69f872>
2012-03-19 15:10:58 +0000

    Наведение порядка в ресурсах

--- trunk/orion-web/src/main/resources/ua/orion/web/ui-interface.js
+++ trunk/orion-web/src/main/resources/ua/orion/web/ui-interface.js
@@ -1,224 +1,197 @@
-//Инициализация классов кнопок, к которым присоеденяются иконки
-//TODO Почему бы не сделать это в виде ассоциативного массива? Пример:
-//var elements={"ui-button-close":"ui-icon-close",
-//              "ui-button-add":"ui-icon-circle-plus"}
-//tfalse в конце используется для того, чтобы сделать кнопку без текста. 
-//var elements = new Array("ui-button-close","ui-button-add","ui-button-refresh","ui-button-login","ui-button-quit","ui-button-del","ui-button-cancel","ui-button-edit-tfalse","ui-button-view-tfalse","ui-button-del-tfalse","ui-button-tolist","ui-button-right-list-tfalse");
-var elements = new Array("ui-button-close","ui-button-add","ui-button-refresh","ui-button-login","ui-button-quit","ui-button-del","ui-button-cancel","ui-button-edit-tfalse","ui-button-view-tfalse","ui-button-del-tfalse","ui-button-tolist","ui-button-right-list-tfalse",
-    "ui-button-detail", "ui-button-edit","ui-button-view","ui-button-del", "ui-button-check");
-//Инициализация классов иконок, для кнопок в массиве elements
-//var icons = new Array("ui-icon-close","ui-icon-circle-plus","ui-icon-refresh","ui-icon-key","ui-icon-power","ui-icon-trash", "ui-icon-cancel", "ui-icon-pencil","ui-icon-print","ui-icon-trash","ui-icon-arrowreturnthick-1-n","ui-icon-triangle-1-e");
-var icons = new Array("ui-icon-close","ui-icon-circle-plus","ui-icon-refresh","ui-icon-key","ui-icon-power","ui-icon-trash", "ui-icon-cancel", "ui-icon-pencil","ui-icon-print","ui-icon-trash","ui-icon-arrowreturnthick-1-n","ui-icon-triangle-1-e", 
-    "ui-icon-zoomin", "ui-icon-pencil", "ui-icon-search", "ui-icon-circle-minus", "ui-icon-circle-check");
+//Декорация UI интерфейса после загрузки страницы и после 
+//обновления зоны
+$J(document).ready(function(){
+    //Инициализация классов кнопок, к которым присоеденяются иконки
+    //TODO Почему бы не сделать это в виде ассоциативного массива? Пример:
+    //var elements={"ui-button-close":"ui-icon-close",
+    //              "ui-button-add":"ui-icon-circle-plus"}
+    //tfalse в конце используется для того, чтобы сделать кнопку без текста. 
+    //var elements = new Array("ui-button-close","ui-button-add","ui-button-refresh","ui-button-login","ui-button-quit","ui-button-del","ui-button-cancel","ui-button-edit-tfalse","ui-button-view-tfalse","ui-button-del-tfalse","ui-button-tolist","ui-button-right-list-tfalse");
+    var elements = new Array("ui-button-close","ui-button-add","ui-button-refresh","ui-button-login","ui-button-quit","ui-button-del","ui-button-cancel","ui-button-edit-tfalse","ui-button-view-tfalse","ui-button-del-tfalse","ui-button-tolist","ui-button-right-list-tfalse",
+        "ui-button-detail", "ui-button-edit","ui-button-view","ui-button-del", "ui-button-check");
+    //Инициализация классов иконок, для кнопок в массиве elements
+    //var icons = new Array("ui-icon-close","ui-icon-circle-plus","ui-icon-refresh","ui-icon-key","ui-icon-power","ui-icon-trash", "ui-icon-cancel", "ui-icon-pencil","ui-icon-print","ui-icon-trash","ui-icon-arrowreturnthick-1-n","ui-icon-triangle-1-e");
+    var icons = new Array("ui-icon-close","ui-icon-circle-plus","ui-icon-refresh","ui-icon-key","ui-icon-power","ui-icon-trash", "ui-icon-cancel", "ui-icon-pencil","ui-icon-print","ui-icon-trash","ui-icon-arrowreturnthick-1-n","ui-icon-triangle-1-e", 
+        "ui-icon-zoomin", "ui-icon-pencil", "ui-icon-search", "ui-icon-circle-minus", "ui-icon-circle-check");
     
-//Инициализация иконок на кнопках
-//ec - класс элемента HTML, ic - класс иконик (из jquery-ui)
-function createIcon(ec, ic){
-    jQuery("."+ec).button({
-        icons: {
-            primary: ic
-        },
-        text: ec.indexOf("tfalse")>-1?false:true
-    })
-}
-//Инициализация кнопок.
-function initializeButtons(){
-    jQuery(".ui-button[role!=button]").button();
-}
-//Инициализация кнопок для Submit
-function initializeSubmits(){
-    //Submit преобразуется в <button type="submit"/>SUBMIT</button>
-    jQuery(".t-beaneditor-row input[type=submit], .ui-submit[role!=button]").each(function(){
-        jQuery(this).replaceWith('<button class="ui-submit" type="' + jQuery(this).attr('type') + '">' + jQuery(this).val() + '</button>');
-    });
-    jQuery(".ui-submit").button({
-        icons: {
-            primary: "ui-icon-circle-check"
+    function initUI(event){
+        //Если event не задан, то применим инициализацию ко всему телу документа,
+        //иначе это инициализация зоны
+        root=!event?$J("body")[0]:this;
+        
+        //Инициализация иконок на кнопках
+        //ec - класс элемента HTML, ic - класс иконик (из $J-ui)
+        function createIcon(ec, ic){
+            $J("."+ec).button({
+                icons: {
+                    primary: ic
+                },
+                text: ec.indexOf("tfalse")>-1?false:true
+            })
         }
-    });
-}
-//Инициализация иконок для кнопок
-function initializeIcons(){
-    for (var i=0; i<elements.length; i++){
-        createIcon(elements[i], icons[i]);
-    }
-}
-//Создание ui-checkbox-ов 
-function initializeCheckboxes(){
-    jQuery("input[type=checkbox]").each(function(){
-        bindCheckBoxes(this);
-    })   
-}
-//Создание ui-текстовых полей
-function initializeTextFields(){
-    jQuery("input[type=text], textarea").each(function(){
-        bindTextElements(this);
-    })
-}
-/**
+        //Инициализация кнопок.
+        function initializeButtons(){
+            $J(".ui-button[role!=button]", root).button();
+        }
+        //Инициализация кнопок для Submit
+        function initializeSubmits(){
+            //Submit преобразуется в <button type="submit"/>SUBMIT</button>
+            $J(".t-beaneditor-row input[type=submit], .ui-submit[role!=button]", root).each(function(){
+                $J(this).replaceWith('<button class="ui-submit" type="' + $J(this).attr('type') + '">' + $J(this).val() + '</button>');
+            });
+            $J(".ui-submit", root).button({
+                icons: {
+                    primary: "ui-icon-circle-check"
+                }
+            });
+        }
+        //Инициализация иконок для кнопок
+        function initializeIcons(){
+            for (var i=0; i<elements.length; i++){
+                createIcon(elements[i], icons[i]);
+            }
+        }
+        //Создание ui-checkbox-ов 
+        function initializeCheckboxes(){
+            $J("input[type=checkbox]", root).each(function(){
+                bindCheckBoxes(this);
+            })   
+        }
+        //Создание ui-текстовых полей
+        function initializeTextFields(){
+            $J("input[type=text], textarea", root).each(function(){
+                bindTextElements(this);
+            })
+        }
+        /**
  * Добавление UI-классов к элементам beaneditor и beandisplay
  */
-function addUIClassesToBeanEditView(){
-    jQuery("DIV.t-beaneditor").addClass("ui-widget-content").removeClass("t-beaneditor");
-    jQuery("dl.t-beandisplay").addClass("ui-widget-content").removeClass("t-beandisplay");
-}
-
-//Инициализация компонентов
-function initializeUIComponents(){
-    addUIClassesToBeanEditView();
-    putCookieThemeToSelect();
-    initializeTextFields();
-    initializeCalendar();
-    initializeButtons();
-    initializeSubmits();
-    initializeIcons();
-    //Создание выпадающего списка из select
-    jQuery("select").selectmenu();
-    initializeCheckboxes();
-    jQuery(".ui-buttonset").buttonset();
-    jQuery("button").parent('a').css({
-        'text-decoration':'none'
-    })
-    applyCookieTheme();
-}
-//Инициализация календаря
-function initializeCalendar(){
-    //Установка локали
-    $J.datepicker.regional['uk'].dateFormat=$J.datepicker.regional['ru'].dateFormat;
-    jQuery.datepicker.setDefaults(jQuery.datepicker.regional[Ori.LOCALE]);
-    //Создание календаря
-    jQuery(".t-calendar-trigger").parent().find("input").datepicker({
-        showButtonPanel: true,  
-//        dateFormat: 'dd.mm.yy',
-        showAnim: 'slideDown',
-        duration: 'slow',
-        showOtherMonths: true,
-        selectOtherMonths: true,
-        showWeek: true,
-        changeMonth: true,
-        changeYear: true,
-        yearRange: '1900:2050'
-    });
-    //Скрытие стандартного вызова календаря от Tapestry
-    jQuery(".t-calendar-trigger").css({
-        'display':'none'
-    });
-}
-
-//Инициализация полей ввода
-function bindTextElements(element){	
-    //Скругление текстовых полей
-    jQuery(element).addClass("ui-state-default ui-corner-all");
-    //При активации текстового поля оно подсвечивается
-    jQuery(element).bind("focusin", function(e){
-        jQuery(this).toggleClass("ui-state-focus ui-state-default");
-    });
-    //При деактивации текстового поля оно теряет подсветку(фокус)
-    jQuery(element).bind("focusout", function(e){
-        jQuery(this).toggleClass("ui-state-focus ui-state-default");
-    });			 
-}
-
-//Инициализация checkbox-ов
-function bindCheckBoxes(element){
-    //Выполняются специальный обрамления
-    jQuery(element).wrap("<sp />");
-    jQuery(element).addClass("ui-state-default");
-    jQuery(element).parent("sp").after("<span />");
-    var parent =  jQuery(element).parent("sp").next();
-    jQuery(element).addClass("ui-helper-hidden");
-    parent.css({
-        width:20,
-        height:20,
-        display:"block"
-    });
-    parent.wrap("<span class='ui-state-default ui-corner-all' style='display:inline-block;width:16px;height:16px;margin-right:5px;'/>");		 
-    parent.parent().addClass('hover');
-    //Выполняется установка начального состояния
-    if (jQuery(element).is(":checked")){
-        parent.addClass("ui-icon ui-icon-check");
-        parent.parent("span").addClass("ui-state-active");
-    }
-    //Обработка нажатия на фиктивный checkbox
-    parent.parent("span").click(function(event){
-        jQuery(this).toggleClass("ui-state-active");
-        parent.toggleClass("ui-icon ui-icon-check");
-        jQuery(element).click();		
-    });		 
-}
-
-//Установка начального значения выбранной темы в select
-function putCookieThemeToSelect(){
-    jQuery("#ui-select-theme option").each(function(){
-        if (jQuery(this).html()==jQuery.cookie("ui-theme")){
-            jQuery(this).attr("selected", "selected");
+        function addUIClassesToBeanEditView(){
+            $J("DIV.t-beaneditor", root).addClass("ui-widget-content").removeClass("t-beaneditor");
+            $J("dl.t-beandisplay", root).addClass("ui-widget-content").removeClass("t-beandisplay");
         }
-    })
-}
 
-//Применение темы
-function applyCookieTheme(){
-    if (jQuery.cookie("ui-theme")!=null){
-        var what = "css/.*/jquery-ui-1.8.16.custom.css";
-        var to = "css/"+jQuery.cookie("ui-theme")+"/jquery-ui-1.8.16.custom.css";
-        jQuery("head link").each(function(){
-            if (jQuery(this).attr("href").indexOf("jquery-ui-1.8.16.custom.css")>-1) {
-                jQuery(this).attr("href",jQuery(this).attr("href").replace(new RegExp (what, 'g'), to));
+        //Инициализация компонентов
+        function initializeUIComponents(){
+            addUIClassesToBeanEditView();
+            initializeTextFields();
+            initializeCalendar();
+            initializeButtons();
+            initializeSubmits();
+            initializeIcons();
+            //Создание выпадающего списка из select
+            $J("select", root).selectmenu();
+            initializeCheckboxes();
+            $J(".ui-buttonset", root).buttonset();
+            $J("button", root).parent('a').css({
+                'text-decoration':'none'
+            })
+        }
+        //Инициализация календаря
+        function initializeCalendar(){
+            //Установка локали
+            $J.datepicker.regional['uk'].dateFormat=$J.datepicker.regional['ru'].dateFormat;
+            $J.datepicker.setDefaults($J.datepicker.regional[Ori.LOCALE]);
+            //Создание календаря
+            $J(".t-calendar-trigger", root).parent().find("input").datepicker({
+                showButtonPanel: true,  
+                //        dateFormat: 'dd.mm.yy',
+                showAnim: 'slideDown',
+                duration: 'slow',
+                showOtherMonths: true,
+                selectOtherMonths: true,
+                showWeek: true,
+                changeMonth: true,
+                changeYear: true,
+                yearRange: '1900:2050'
+            });
+            //Скрытие стандартного вызова календаря от Tapestry
+            $J(".t-calendar-trigger", root).css({
+                'display':'none'
+            });
+        }
+
+        //Инициализация полей ввода
+        function bindTextElements(element){	
+            //Скругление текстовых полей
+            $J(element).addClass("ui-state-default ui-corner-all");
+            //При активации текстового поля оно подсвечивается
+            $J(element).bind("focusin", function(e){
+                $J(this).toggleClass("ui-state-focus ui-state-default");
+            });
+            //При деактивации текстового поля оно теряет подсветку(фокус)
+            $J(element).bind("focusout", function(e){
+                $J(this).toggleClass("ui-state-focus ui-state-default");
+            });			 
+        }
+
+        //Инициализация checkbox-ов
+        function bindCheckBoxes(element){
+            //Выполняются специальный обрамления
+            $J(element).wrap("<sp />");
+            $J(element).addClass("ui-state-default");
+            $J(element).parent("sp").after("<span />");
+            var parent =  $J(element).parent("sp").next();
+            $J(element).addClass("ui-helper-hidden");
+            parent.css({
+                width:20,
+                height:20,
+                display:"block"
+            });
+            parent.wrap("<span class='ui-state-default ui-corner-all' style='display:inline-block;width:16px;height:16px;margin-right:5px;'/>");		 
+            parent.parent().addClass('hover');
+            //Выполняется установка начального состояния
+            if ($J(element).is(":checked")){
+                parent.addClass("ui-icon ui-icon-check");
+                parent.parent("span").addClass("ui-state-active");
             }
-        });
-    }
-}
-/**
+            //Обработка нажатия на фиктивный checkbox
+            parent.parent("span").click(function(event){
+                $J(this).toggleClass("ui-state-active");
+                parent.toggleClass("ui-icon ui-icon-check");
+                $J(element).click();		
+            });		 
+        }
+
+        /**
  * Добавление UI-классов к элементам интерфейса
  */
-function updateCSS(){
-    //jQuery("table.t-data-grid tbody tr td[class!='action']").addClass("ui-widget-content");
-    jQuery("table.t-data-grid thead tr th[class!='action t-last']").addClass("ui-state-default");
-    jQuery("div.t-data-grid-pager a").addClass("ui-corner-all ui-state-default");
-    jQuery("div.t-data-grid-pager span.current").addClass("ui-corner-all ui-state-active");
-    jQuery(".ui-grid-cell-action-tip").addClass("ui-corner-all ui-state-default");
-}
+        function updateCSS(){
+            //$J("table.t-data-grid tbody tr td[class!='action']").addClass("ui-widget-content");
+            $J("table.t-data-grid thead tr th[class!='action t-last']").addClass("ui-state-default");
+            $J("div.t-data-grid-pager a").addClass("ui-corner-all ui-state-default");
+            $J("div.t-data-grid-pager span.current").addClass("ui-corner-all ui-state-active");
+            $J(".ui-grid-cell-action-tip").addClass("ui-corner-all ui-state-default");
+        }
 
-/*
+        /*
  * Создание подсказок для элементов. 
  * Используется модифицированный плагин Easy Tooltip 1.0
- * @param функция easyTooltip вызывается на элементах, на которые необходимо
+ * функция easyTooltip вызывается на элементах, на которые необходимо
  * назначить подсказки и параметром в ней является текстовая строка, которая 
  * служит для идентификации типа элемента. 
  */
-function createToolTips(){
-    if (Ori.SHOW_HINTS){
-        jQuery("input[type=text]").easyTooltip({
-            ctype:"input[type=text]"
-        });
-        jQuery("select").easyTooltip({
-            ctype:"select"
-        });
-    }
-}
-//Декорация UI интерфейса после загрузки страницы и после 
-//обновления зоны
-jQuery(document).ready(function(){
-    var initUI=function(e){
+        function createToolTips(){
+            if (Ori.SHOW_HINTS){
+                $J("input[type=text]").easyTooltip({
+                    ctype:"input[type=text]"
+                });
+                $J("select").easyTooltip({
+                    ctype:"select"
+                });
+            }
+        }
+        
         //Инициализация ui-интерфейса
-        if (jQuery("ui#interface").text()=="true"){
+        if ($J("ui#interface").text()=="true"){
             initializeUIComponents();   
         }
         updateCSS();
         //Создание подсказок
         createToolTips();
     }
+    
+    //Регистация слушателя событие на обновление зоны
     Ori.Event.bind('.t-zone', Tapestry.ZONE_UPDATED_EVENT, initUI);
-    initUI($J("body"));
+    initUI();
 });
-
-//При полностью загруженной структуре DOM
-jQuery(document).ready(function(){
-    jQuery("#select-theme-button").click(function(){
-        jQuery.cookie("ui-theme", jQuery("#ui-select-theme option:selected").text());
-        applyCookieTheme();
-    })
-    jQuery("#ui-show-select-theme-dialog-button").click(function(){
-        jQuery("#ui-select-theme-dialog").dialog()
-    })
-})
